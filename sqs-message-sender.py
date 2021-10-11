09754f14da40ac3bf7c873407f7c95a83ede138b
Ubuntu <ubuntu@ip-172-31-20-39.us-west-2.compute.internal>
2021-10-11 20:05:13 +0000

    updating examples

--- sqs-message-sender.py
+++ sqs-message-sender.py
@@ -3,11 +3,15 @@ import boto3
 sqs = boto3.resource('sqs')
 
 # Retrieving a queue by its name
-queue = sqs.get_queue_by_name(QueueName='test')
+#queue = sqs.get_queue_by_name(QueueName='standard-queue')
+queue = sqs.get_queue_by_name(QueueName='queue.fifo')
 
 # Create a new message
-response = queue.send_message(MessageBody='world')
+#Sending 1-20
+for number in range(1, 21):
+    print(f"Sending {number}")
+    response = queue.send_message(MessageBody=str(number), MessageGroupId="reqGroup", MessageDeduplicationId = str(number))
 
-# The response is not a resource, but gives you a message ID and MD5
-print("MessageId created: {0}".format(response.get('MessageId')))
-print("MD5 created: {0}".format(response.get('MD5OfMessageBody')))
+    # The response is not a resource, but gives you a message ID and MD5
+    print("MessageId created: {0}".format(response.get('MessageId')))
+    print("MD5 created: {0}".format(response.get('MD5OfMessageBody')))
